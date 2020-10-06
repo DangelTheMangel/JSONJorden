2c9cfb9f140aab1c9157941c7c1a2656fd2fb2ad
Christian Randleff <cra@minuba.dk>
2020-10-06 15:27:16 +0200

    ja nu med  navn

--- JSONkoden/JSONkoden/src/main/java/main.java
+++ JSONkoden/JSONkoden/src/main/java/main.java
@@ -8,11 +8,11 @@ public class main  extends PApplet {
     public static void main(String[] args) { PApplet.main("main"); }
     float angle;
 
-    JSONObject Jay;
-
+    JSONObject jay;
+    JSONObject jay2;
     Table table;
 
-    float r = 150;
+    float r = 100;
 
 
 
@@ -33,14 +33,16 @@ public class main  extends PApplet {
 
         table = loadTable("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.csv", "header"); //der DER DER WAAHH WAAA SE MIG ***********************************
 
-        Jay = loadJSONObject("https://www.n2yo.com/rest/v1/satellite/positions/25544/41.702/-76.014/0/2/&apiKey=EVZW8Y-GY7GW3-U6FV47-4KH5");
+        jay = loadJSONObject("https://www.n2yo.com/rest/v1/satellite/positions/25544/41.702/-76.014/0/2/&apiKey=EVZW8Y-GY7GW3-U6FV47-4KH5");
+        jay2 = loadJSONObject("https://www.n2yo.com/rest/v1/satellite/positions/25635/41.702/-76.014/0/2/&apiKey=EVZW8Y-GY7GW3-U6FV47-4KH5");
 
 
         noStroke();
 
         globe = createShape(SPHERE, r);
-
         globe.setTexture(earth);
+        System.out.println(jay.getJSONArray("positions"));
+        System.out.println(jay2.getJSONArray("positions"));
     }
 
     @Override
@@ -49,6 +51,7 @@ public class main  extends PApplet {
 
         translate((float) (width*0.5), (float)(height*0.5));
 
+        rotateZ(angle);
         rotateY(angle);
 
 
@@ -68,82 +71,67 @@ public class main  extends PApplet {
 
 
 
+
         for (TableRow row : table.rows()) {
 
-            JSONArray positions = Jay.getJSONArray("positions");
-
-            JSONObject o = positions.getJSONObject(0);
-
-            float lat = o.getFloat("satlatitude");
-
-            float lon = o.getFloat("satlongitude");
-
-
-            // original version
-
-            // float theta = radians(lat) + PI/2;
-
-
-
-            // fix: no + PI/2 needed, since latitude is between -180 and 180 deg
-
-            float theta = radians(lat);
-
-
-
-            float phi = radians(lon) + PI;
-
-
-
-            // original version
-
-            // float x = r * sin(theta) * cos(phi);
-
-            // float y = -r * sin(theta) * sin(phi);
-
-            // float z = r * cos(theta);
-
-
-
-            // fix: in OpenGL, y & z axes are flipped from math notation of spherical coordinates
-
-            float x = r * cos(theta) * cos(phi)-40;
-
-            float y = -r * sin(theta);
-
-            float z = -r * cos(theta) * sin(phi);
-
-
-
-            PVector pos = new PVector(x, y, z);
-
-            PVector xaxis = new PVector(1, 0, 0);
-
-            float angleb = PVector.angleBetween(xaxis, pos);
-
-            PVector raxis = xaxis.cross(pos);
-
-
-
-
-
-
-
-            pushMatrix();
-
-            translate(x, y, z);
-
-            rotate(angleb, raxis.x, raxis.y, raxis.z);
-
-            fill(255);
-
-            box(10, 10, 10);
-
-            popMatrix();
+         drawBox(jay2, jay2);
+         drawBox(jay,jay);
 
         }
     }
 
+    public void drawBox(JSONObject json, JSONObject json2){
+        JSONArray positions = json.getJSONArray("positions");
+
+        JSONObject o = positions.getJSONObject(0);
+
+        float lat = o.getFloat("satlatitude");
+
+        float lon = o.getFloat("satlongitude");
+
+        float theta = radians(lat);
+
+        float phi = radians(lon) + PI;
+
+        float x = r * cos(theta) * cos(phi)-40;
+
+        float y = -r * sin(theta);
+
+        float z = -r * cos(theta) * sin(phi);
+
+
+
+        PVector pos = new PVector(x, y, z);
+
+        PVector xaxis = new PVector(1, 0, 0);
+
+        float angleb = PVector.angleBetween(xaxis, pos);
+
+        PVector raxis = xaxis.cross(pos);
+
+
+
+
+
+
+        //
+        if(mousePressed){
+        JSONObject info = json2.getJSONObject("info");
+        String name = info.getString("satname");
+        int id = info.getInt("satid");
+        String tag = name + ": " + id;
+        pushMatrix();
+
+        translate(x, y, z);
+
+        rotate(angleb, raxis.x, raxis.y, raxis.z);
+
+        fill(255);
+        text( tag ,20,20,20);
+        box(20, 20, 20);
+
+        popMatrix();}
+    }
 
 }
 
