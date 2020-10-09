cd0a6932ef7a10974a0e21742b14c6d5704d3d82
Christian Randleff <cra@minuba.dk>
2020-10-09 14:00:35 +0200

    virker nu

--- JSONkoden/JSONkoden/src/main/java/main.java
+++ JSONkoden/JSONkoden/src/main/java/main.java
@@ -2,39 +2,36 @@ import processing.core.*;
 import processing.data.JSONArray;
 import processing.data.JSONObject;
 import processing.data.Table;
-import processing.data.TableRow;
 
 public class main  extends PApplet {
     public static void main(String[] args) { PApplet.main("main"); }
-    float angle;
-
+    float angleX,angleY,angleZ;
+    String info;
     JSONObject jay;
     JSONObject jay2;
     Table table;
-
+    boolean show;
     float r = 100;
+    AlmindeligKnap btnHowTo;
 
-
-
+    PImage bg ;
     PImage earth;
 
     PShape globe;
 
     @Override
     public void settings() {
-        size(600, 600,P3D);
+        size(1280, 720,P3D);
     }
 
     @Override
     public void setup() {
+        bg = loadImage("w.jpg");
         earth = loadImage("earth.jpg"); //prøv Albert.png og b4nny.png og earth.jpg
-
-        // table = loadTable("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_day.csv", "header");
-
-        table = loadTable("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.csv", "header"); //der DER DER WAAHH WAAA SE MIG ***********************************
-
+        //table = loadTable("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.csv", "header"); //der DER DER WAAHH WAAA SE MIG ***********************************
         jay = loadJSONObject("https://www.n2yo.com/rest/v1/satellite/positions/25544/41.702/-76.014/0/2/&apiKey=EVZW8Y-GY7GW3-U6FV47-4KH5");
         jay2 = loadJSONObject("https://www.n2yo.com/rest/v1/satellite/positions/25635/41.702/-76.014/0/2/&apiKey=EVZW8Y-GY7GW3-U6FV47-4KH5");
+        info = "Shown satellites: \n" + hentText(jay) + "\n" + hentText(jay2);
 
 
         noStroke();
@@ -43,41 +40,56 @@ public class main  extends PApplet {
         globe.setTexture(earth);
         System.out.println(jay.getJSONArray("positions"));
         System.out.println(jay2.getJSONArray("positions"));
+
+
+        noStroke();
+
+        // init them: (xPos, yPos, width, height)
     }
 
     @Override
     public void draw() {
-        background(51);
-
-        translate((float) (width*0.5), (float)(height*0.5));
-
-        rotateZ(angle);
-        rotateY(angle);
-
-
-        angle += 0.05;
-
-
-
-        lights();
-
-        fill(200);
-
-        noStroke();
-
-        //sphere(r);
-
-        shape(globe);
-
-
-
-
-        for (TableRow row : table.rows()) {
-
-         drawBox(jay2, jay2);
-         drawBox(jay,jay);
+        clear();
+        background(bg);
+        if (frameCount % 10000 == 0) {
+            jay = loadJSONObject("https://www.n2yo.com/rest/v1/satellite/positions/25544/41.702/-76.014/0/2/&apiKey=EVZW8Y-GY7GW3-U6FV47-4KH5");
+            jay2 = loadJSONObject("https://www.n2yo.com/rest/v1/satellite/positions/25635/41.702/-76.014/0/2/&apiKey=EVZW8Y-GY7GW3-U6FV47-4KH5");
 
         }
+
+        lights();
+        fill(250,150,0);
+        textSize(35);
+        text(info,50,50);
+        text("Press the mouse to show satellite names \nPress and hold the arrow keys to turn the planet",50, height-80);
+        textSize(23);
+        translate((float) (width*0.5), (float)(height*0.5));
+        if (keyPressed && keyCode == UP) {
+            angleX += 0.05;
+        }
+        if (keyPressed && keyCode == DOWN) {
+            angleX -= 0.05;
+        }
+        if (keyPressed && keyCode == RIGHT) {
+            angleY += 0.05;
+        }
+        if (keyPressed && keyCode == LEFT) {
+            angleY -= 0.05;
+        }
+
+        rotateX(angleX);
+        rotateZ(angleZ);
+        rotateY(angleY);
+
+
+
+        fill(200);
+        noStroke();
+        shape(globe);
+        drawBox(jay2, jay2);
+        drawBox(jay,jay);
+
+
     }
 
     public void drawBox(JSONObject json, JSONObject json2){
@@ -93,11 +105,11 @@ public class main  extends PApplet {
 
         float phi = radians(lon) + PI;
 
-        float x = r * cos(theta) * cos(phi)-40;
+        float x = r*2 * cos(theta) * cos(phi)-40;
 
-        float y = -r * sin(theta);
+        float y = -r*2 * sin(theta);
 
-        float z = -r * cos(theta) * sin(phi);
+        float z = -r*2 * cos(theta) * sin(phi);
 
 
 
@@ -114,25 +126,36 @@ public class main  extends PApplet {
 
 
 
-        //
-        if(mousePressed){
-        JSONObject info = json2.getJSONObject("info");
-        String name = info.getString("satname");
-        int id = info.getInt("satid");
-        String tag = name + ": " + id;
         pushMatrix();
-
         translate(x, y, z);
 
         rotate(angleb, raxis.x, raxis.y, raxis.z);
 
-        fill(255);
-        text( tag ,20,20,20);
-        box(20, 20, 20);
 
-        popMatrix();}
+        if(show == false){
+
+
+
+            fill(250,250,0);
+            text( hentText(json) ,20,20,20);
+
+
+            }fill(255);
+        box(20, 20, 20);
+        popMatrix();
+    }
+    public String hentText(JSONObject json2){
+        JSONObject info = json2.getJSONObject("info");
+        String name = info.getString("satname");
+        int id = info.getInt("satid");
+        String tag = name + ": " + id;
+        return tag;
     }
 
+    @Override
+    public void mouseClicked() {
+
+            show = !show;
+
+    }
 }
-
-
